f1084b5db4fb238907736f30c4364ad4e95af06d
yanguadotdev <yanguasamir05@gmail.com>
2025-05-30 16:04:31 -0500

    Enhance text reveal animation with new data attributes and CSS styles for improved visibility

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
-import { $$ } from "./utils.js"
+import { $$ } from './utils.js'
 
 // Text Reveal Animation
-const textReveals = [...$$('.text__reveal')]
+const textReveals = [...$$('.text__reveal'), ...$$('.text__reveal--full')]
 
 let callback = (entries) => {
   entries.forEach((entry) => {
+    const target = entry.target
+    const animateFor = target.dataset.animate
     if (entry.isIntersecting) {
-      ;[...entry.target.querySelectorAll('span')].forEach((span, idx) => {
-        setTimeout(() => {
-          span.style.transform = 'translateY(0)'
-        }, (idx + 1) * 50)
-      })
+      if (animateFor === 'letters') {
+        ;[...entry.target.querySelectorAll('span')].forEach((span, idx) => {
+          setTimeout(() => {
+            span.style.transform = 'translateY(0)'
+          }, (idx + 1) * 50)
+        })
+      } else if (animateFor === 'line') {
+        target.firstElementChild.style.transform = 'translateY(0)'
+      }
     }
   })
 }
@@ -22,11 +28,13 @@ let options = {
 
 let observer = new IntersectionObserver(callback, options)
 textReveals.forEach((text) => {
-  let string = text.innerText
-  let html = ''
-  for (let i = 0; i < string.length; i++) {
-    html += `<span>${string[i]}</span>`
+  if (text.dataset.animate === 'letters') {
+    let string = text.innerText
+    let html = ''
+    for (let i = 0; i < string.length; i++) {
+      html += `<span>${string[i]}</span>`
+    }
+    text.innerHTML = html
   }
-  text.innerHTML = html
   observer.observe(text)
 })
